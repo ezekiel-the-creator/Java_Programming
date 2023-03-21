02496141d5878c149d646414997542cb07da7526
Zak-Rey <Zaktripple@gmail.com>
2023-03-21 18:51:25 +0300

    Added iteration statements for control flow in java

--- src/main/java/javacode/Java_Programming/java_intro/ControlFlow.java
+++ src/main/java/javacode/Java_Programming/java_intro/ControlFlow.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class ControlFlow {
     public static void main(String... args){
         selection();
+        iteration();
     }
 
     private static void selection(){
@@ -14,6 +15,7 @@ public class ControlFlow {
         int y = 2;
         System.out.println(x);
 
+        //if else, if else if else, if
         if (x > y) {
             System.out.println("X is greater than y");
         } else {
@@ -27,6 +29,7 @@ public class ControlFlow {
         } else {
             System.out.println("y is greater than x");
         }
+
         //Switch statement
         switch (x) {
             case 1:
@@ -63,5 +66,54 @@ public class ControlFlow {
             default -> System.out.println("x is not 1,3,4,5,6");
         }
     }
+    
+    //Iteration statements: while, do while, for, 
+    public static void iteration(){
+        System.out.println("\nIteration statements");
 
+        int n = 0;
+        while(n > 5){
+            System.out.println(n + " ");
+            n++;
+        }
+
+        System.out.println();
+
+        n = 0;
+        do {
+            System.out.println(n + " ");
+            n++;
+        } while(n < 5);
+        System.out.println();
+
+        for (int i = 0; i < 3; i++){
+            System.out.println(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i > -1; i++){
+            System.out.println(i + " ");
+            if(i > 1) break;
+        }
+
+        System.out.println("\n\nLoop 1:");
+        for (int i = 0, j = 0; i < 3 && j < 3; ++i, ++j){
+            System.out.println(i + " " + j);
+        }
+
+        System.out.println("\nLoop 2:");
+        for (int x = getInitiatialValue(), i=x == -3 ? x + 3 : 0, j = 0; i < 4 || j < 4 ; ++i, j = i){
+            System.out.println(i + " " + j);
+        }
+
+        System.out.println("\nIterate an array");
+        int[] arr = {13, 14, 15, 16, 17, 18};
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static int getInitiatialValue() {
+        return 0;
+    }
 }
