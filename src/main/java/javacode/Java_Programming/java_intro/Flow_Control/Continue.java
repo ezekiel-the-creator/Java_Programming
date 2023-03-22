cd536f71fa480654c55eee4094f066c6538d7d22
Zak-Rey <Zaktripple@gmail.com>
2023-03-22 10:43:27 +0300

    break branching statement usage in java

---
+++ src/main/java/javacode/Java_Programming/java_intro/Flow_Control/Continue.java
@@ -0,0 +1,16 @@
+package javacode.Java_Programming.java_intro.Flow_Control;
+
+public class Continue {
+    public static void main(String... args){
+        int[] numbers = {1,2, 3, 4, 5};
+
+        for (int i : numbers){
+            if (numbers[i] % 2 == 0){
+                System.out.println("Skipping even number: " + numbers[i]);
+                continue;
+            }
+
+            System.out.println("Current number is: " + numbers[i]);
+        }
+    }
+}
