de73d6c549c91d8804c2e2dfe8559664567e33e2
Zak-Rey <Zaktripple@gmail.com>
2023-03-22 11:27:48 +0300

    if-else-if usage java

---
+++ src/main/java/javacode/Java_Programming/java_intro/Flow_Control/IfElseifElse.java
@@ -0,0 +1,17 @@
+package javacode.Java_Programming.java_intro.Flow_Control;
+
+public class IfElseifElse {
+    public static void main(String... args){
+        int age = 100000;
+
+        if (age < 18){
+            System.out.println("You are a minor.");
+        }else if(age >= 18 && age < 21){
+            System.out.println("You are a young adult.");
+        }else if(age>=21 && age < 65){
+            System.out.println("You are an adult.");
+        }else{
+            System.out.println("You are a senior citizen.");
+        }
+    }
+}
