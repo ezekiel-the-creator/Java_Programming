9b53821d3562cea4c7bb22bfe627af6cddafae68
Zak-Rey <zaktripple@gmail.com>
2023-03-24 20:23:32 +0800

    String Concatenation

---
+++ src/main/java/javacode/Java_Programming/java_intro/String_Class/ConcatenateString.java
@@ -0,0 +1,17 @@
+package javacode.Java_Programming.java_intro.String_Class;
+
+public class ConcatenateString {
+    public static void main(String... args){
+        String firstName = "Ezekiel";
+        String LastName = "Kiprotich";
+
+        //Concatenation using `+` operator
+        String fullName = firstName + " " +LastName;
+
+        //Concatenation using   `concat()` method
+        String fullName1 = firstName.concat(" ").concat(LastName);
+
+        System.out.println(fullName);
+        System.out.println(fullName1);
+    }
+}
