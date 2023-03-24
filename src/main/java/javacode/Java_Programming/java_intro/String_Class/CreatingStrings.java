90f0edb378be9461c80b91a04b3b6e673b8ff429
Zak-Rey <zaktripple@gmail.com>
2023-03-24 20:16:00 +0800

    Creating a String Object

---
+++ src/main/java/javacode/Java_Programming/java_intro/String_Class/CreatingStrings.java
@@ -0,0 +1,13 @@
+package javacode.Java_Programming.java_intro.String_Class;
+
+public class CreatingStrings {
+    public static void main(String... args){
+        //Creating a new String object
+        //Using String Literal
+        String greeting = "Hello, world!";
+        System.out.println(greeting);
+        //Using `new` keyword
+        String name = new String("Zak");
+        System.out.println(name);
+    }
+}
