c4055e8714758099d0b0a40e87a90da1e3335806
Zak-Rey <zaktripple@gmail.com>
2023-03-26 17:28:11 +0300

    Interface Implemention

--- oop_java/interface/InterfaceDemo.java
+++ oop_java/interface/InterfaceDemo.java
@@ -1,12 +1,13 @@
 public class interfaceDemo {
     public static void main(String... args){
+
         Shape circle = new Circle(5);
         Shape rectangle = new Rectangle(3, 4);
 
-        System.out.println("circle area: " + circle.getArea());
+        System.out.println("circle area: " + circle.getArea());//getArea method defined in class Circle is implemented
         System.out.println("Circle perimete: " + circle.getPerimeter());
 
-        System.out.println("Rectangle area: " + rectangle.getArea());
+        System.out.println("Rectangle area: " + rectangle.getArea());//getArea method defined in class Rectanle is implemented
         System.out.println("Rectangle perimeter: " + rectangle.getPerimeter());
     }
 }
