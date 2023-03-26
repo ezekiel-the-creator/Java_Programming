5c547edbf04179db79e171f7052ee34cbb401efa
Zak-Rey <zaktripple@gmail.com>
2023-03-26 16:38:13 +0800

    Inheritance

---
+++ src/main/java/javacode/Java_Programming/oop_java/Inheritance/Car.java
@@ -0,0 +1,24 @@
+package javacode.Java_Programming.oop_java.Inheritance;
+
+public class Car extends Vehicle {
+    private int numDoors;
+
+    public Car(String make, String model, int year, int numDoors){
+        super(make, model, year);
+        this.numDoors = numDoors;
+    }
+
+    public int getNumDoors(){
+        return numDoors;
+    }
+
+    public void setNumDoors(int numDoors){
+        this.numDoors = numDoors;
+    }
+
+    public static void main(String... args){
+        Car newCar = new Car("Nissan", "GTR", 2022, 4);
+        newCar.drive(123);
+        newCar.stop();
+    }
+}
