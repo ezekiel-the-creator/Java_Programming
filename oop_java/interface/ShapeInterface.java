d8f71b749330aba4d0358d10c00b8f83d0225dcf
Zak-Rey <zaktripple@gmail.com>
2023-03-26 17:32:52 +0300

    Java interface

--- oop_java/interface/ShapeInterface.java
+++ oop_java/interface/ShapeInterface.java
@@ -1,3 +1,5 @@
+//An interface is a collection of abstract methods and constant variables that must be implemented by any class that uses the interface.
+
 interface ShapeInterface{
     double getArea();
     double getPerimeter();
