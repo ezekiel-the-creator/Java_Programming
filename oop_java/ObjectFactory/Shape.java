7a73f07f1507587c609b2e86844479d047753d7f
Zak-Rey <zaktripple@gmail.com>
2023-03-27 19:54:23 +0300

    Object Factory

--- oop_java/ObjectFactory/Shape.java
+++ oop_java/ObjectFactory/Shape.java
@@ -1,3 +1,5 @@
+//An object factory is a design pattern that provides a way to create objects without exposing the creation logic to the calling code.
+
 public interface Shape {
     void draw();
 }
