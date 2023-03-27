e0e8bd6ebbea39ae1923d280facb1c490c06e68b
Zak-Rey <zaktripple@gmail.com>
2023-03-27 17:58:20 +0300

    Method overriding

--- oop_java/MethodOverriding/Animal.java
+++ oop_java/MethodOverriding/Animal.java
@@ -1,3 +1,5 @@
+//Method overriding is the ability to provide a new implementation for a method in a subclass that has the same name, return type and parameters as the method in the superclass.
+
 public class Animal {
     public void makeSound(){
         System.out.println("The animal makes a sound.");
