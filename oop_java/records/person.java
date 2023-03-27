f39f68f92d78eba20cd42781f1c55f2e004b74eb
Zak-Rey <zaktripple@gmail.com>
2023-03-27 19:17:31 +0300

    records

--- oop_java/records/person.java
+++ oop_java/records/person.java
@@ -1,1 +1,4 @@
+//records are a new feature that provides a compact syntax for declaring classes that are primarily used to hold data.
+//available for java16 and later versions
+
 public record Person(String name, int age){}
