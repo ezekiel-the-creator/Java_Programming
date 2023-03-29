3a269f940c1a38c43f1ec7c6186713d0f72bb7c3
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:43:38 +0300

    sealed classes and interfaces

---
+++ src/main/java/oop_java/sealed_interfaces/Square.java
@@ -0,0 +1,17 @@
+package oop_java.sealed_interfaces;
+
+/**public final class Square implements Shape {
+  *  public Square(double side){
+   * }
+*
+ *   public double getArea(double side){
+  *      double Area = side * side;
+   *     return Area;
+    *}
+*
+ *   public double getPerimeter(double side){
+  *      double Perimeter = 4 * side;
+   *    return Perimeter;
+    *}
+*}
+*/
