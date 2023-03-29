3f4783d507cf6fc7cd8bab0b47e56a941570376f
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:36:01 +0300

    Final keyword

---
+++ src/main/java/oop_java/FinalKeyword/Vehicle.java
@@ -0,0 +1,16 @@
+package oop_java.FinalKeyword;
+
+//final methods cannot be overriden by a subclass
+
+public class Vehicle {
+    int weight;
+    int baseArea;
+
+    public final int speedometer(){
+        int speed = weight* baseArea;
+
+        return speed;
+    }
+}
+
+//trying to override speedometer() will result to a compile time error.
