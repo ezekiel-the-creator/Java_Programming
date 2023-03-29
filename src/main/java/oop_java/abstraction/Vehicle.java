2323c7c6def885c07f56956f919b8a131845f9ee
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:34:19 +0300

    Abstraction

---
+++ src/main/java/oop_java/abstraction/Vehicle.java
@@ -0,0 +1,19 @@
+package oop_java.abstraction;
+
+public abstract class Vehicle {
+    private String make;
+    private String model;
+    private int year;
+
+    public Vehicle(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public abstract void start();
+    public abstract void stop();
+    public void drive(int speed){
+        System.out.println("Driving the vehicle at " + speed + " mph...");
+    }
+}
