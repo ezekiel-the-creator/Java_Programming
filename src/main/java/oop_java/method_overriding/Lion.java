466cd5d1a4d6f24dd16137ffef074e4f0bb54a2e
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:39:40 +0300

    method overriding

---
+++ src/main/java/oop_java/method_overriding/Lion.java
@@ -0,0 +1,9 @@
+package oop_java.method_overriding;
+
+public class Lion extends Animal {
+    
+    @Override
+    public void makeSound(){
+        System.out.println("The Lion roars.");
+    }
+}
