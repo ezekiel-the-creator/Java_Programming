f0822865c51122e5fe2d64000c5fccfefd51c1b4
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:24:16 +0300

    if else java

--- oop_java/sealedinterfaces/Shape.java
+++
@@ -1,7 +0,0 @@
-//Shape, a sealed interface that perimits only Circle and Square classes to implement it.
-
-public sealed interface Shape permits Circle, Square{
-    //...
-}
-
-//sealed interfaces provide better control over interface hierarchies in java
