3a2ff49a679348400f618bd128a04d9c330c0d3e
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:33:29 +0300

    Java Strings

---
+++ src/main/java/java_intro/String_Class/CreateStrings.java
@@ -0,0 +1,13 @@
+package java_intro.String_Class;
+
+public class CreateStrings {
+    public static void main(String... args){
+        //Creating a new String object
+        //Using String Literal
+        String greeting = "Hello, world!";
+        System.out.println(greeting);
+        //Using `new` keyword
+        String name = new String("Ezekiel");
+        System.out.println(name);
+    }
+}
