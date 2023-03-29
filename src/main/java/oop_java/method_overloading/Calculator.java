9068c4f449c805c2d15c731d0eceb27eff0766a5
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:38:49 +0300

    method overloading

---
+++ src/main/java/oop_java/method_overloading/Calculator.java
@@ -0,0 +1,17 @@
+package oop_java.method_overloading;
+
+//Method overloading is ability to define multiple methods with the same name in a single class, but with different parameters.
+
+public class Calculator {
+    public int add (int a, int b){
+        return a + b;
+    }
+
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    public int add(int a, int b, int c){
+        return a + b + c;
+    }
+}
