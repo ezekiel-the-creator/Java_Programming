874c9cb26ebf23697d7eb8a1414230d60baab641
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:41:31 +0300

    object factory

---
+++ src/main/java/oop_java/object_factory/ShapeFactory.java
@@ -0,0 +1,19 @@
+package oop_java.object_factory;
+public class ShapeFactory {
+    public static Shape createShape(String shapeType){
+        if (shapeType.equalsIgnoreCase("circle")){
+            return new Circle();
+        }else if(shapeType.equalsIgnoreCase("rectangle")){
+            return new Rectangle();
+        }else{
+            throw new IllegalArgumentException("Invalid shape type: " + shapeType);
+        }
+    }
+
+    public static void main(String[] args) {
+        Shape circle = ShapeFactory.createShape("circle");
+        Shape rectangle = ShapeFactory.createShape("rectangle");
+        
+        System.out.println(circle);
+    }
+}
