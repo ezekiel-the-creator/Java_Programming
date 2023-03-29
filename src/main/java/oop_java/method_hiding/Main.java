be4365123b02ca4781a4a317689eddfa8b9aacac
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:38:32 +0300

    method hiding

---
+++ src/main/java/oop_java/method_hiding/Main.java
@@ -0,0 +1,14 @@
+package oop_java.method_hiding;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.makeSound();  //The animal makes a sound.
+
+        Dog dog = new Dog();
+        dog.makeSound();     //The dog barks.
+
+        Animal anotherDog = new Dog();
+        anotherDog.makeSound();// The animal makes a sound.
+    }
+}
