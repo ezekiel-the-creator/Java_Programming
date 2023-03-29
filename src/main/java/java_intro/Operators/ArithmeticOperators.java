552bb8a4d836908259eed3b48fd2f4e84ba6b20f
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:27:53 +0300

    Arithmetic operators

---
+++ src/main/java/java_intro/Operators/ArithmeticOperators.java
@@ -0,0 +1,21 @@
+package java_intro.Operators;
+
+//used to perform mathematical operations like addition, subtraction, multiplication, division and modulus.
+public class ArithmeticOperators {
+    public static void main(String... args){
+    int a = 10;
+    int b = 5;
+
+    int sum = a + b;
+    int diff = a -b;
+    int product = a * b;
+    int quotient = a/b;
+    int reminder = a%b;
+
+    System.out.println("Sum = " + sum);
+    System.out.println("difference = " + diff);
+    System.out.println("product = " + product);
+    System.out.println("quotient = " + quotient);
+    System.out.println("reminder = " + reminder);
+    }
+}
