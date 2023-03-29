899baef6e3934dc71fb887391579042ce181200a
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:39:53 +0300

    method overriding

---
+++ src/main/java/oop_java/method_overriding/Main.java
@@ -0,0 +1,14 @@
+package oop_java.method_overriding;
+
+public class Main {
+    public static void main(String... args){
+
+        Animal animal = new Animal();
+        animal.makeSound();
+
+        Lion lion = new Lion();
+        lion.makeSound();//The method in the subclass Lion gets implemented here.
+    }
+}
+
+//A private methods can neither be inherited or overriden.
