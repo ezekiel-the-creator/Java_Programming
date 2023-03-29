9c68ff41a97e8a2f8f62cd5ae772229e3882db3b
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:42:06 +0300

    records

---
+++ src/main/java/oop_java/records/Person.java
@@ -0,0 +1,16 @@
+package oop_java.records;
+
+//records are a new feature that provides a compact syntax for declaring classes that are primarily used to hold data.
+//available for java16 and later versions
+
+//public record Person(String name, int age){}
+
+//the declaration does not include any constructors or methods, these are generated automatically by the compiler based on the record's fields.
+
+//creating an object syntax
+
+//Person Zak = new Person("Zak", 20);
+
+//Accessing data
+
+//System.out.println(Zak.name());
