78da822bde774729c8832db0385106ac6f8a70b0
Zak-Rey <zaktripple@gmail.com>
2023-03-29 19:32:18 +0300

    java operators

---
+++ src/main/java/java_intro/Operators/Final.java
@@ -0,0 +1,12 @@
+package java_intro.Operators;
+
+public class Final{
+    final int x = 3;
+    final Double PI = 3.14;
+
+    public static void main(String... args){
+        Final obj = new Final();
+        System.out.println(obj.x);
+        System.out.println(obj.PI);
+    }
+}
