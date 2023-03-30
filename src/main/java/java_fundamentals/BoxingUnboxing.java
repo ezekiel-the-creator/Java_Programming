ccecd8799cef61b7375a51fe175bb1f6161fc49c
Zak-Rey <zaktripple@gmail.com>
2023-03-30 10:01:10 +0300

    boxing and unboxing java

---
+++ src/main/java/java_fundamentals/BoxingUnboxing.java
@@ -0,0 +1,20 @@
+package java_fundamentals;
+
+//boxing -> converting primitive data types to corresponding wrapper class(object type)
+//unboxing -> converting an object of wrapper class back to its corresponding primitive type.
+
+public class BoxingUnboxing{
+    public static void main(String... args){
+        boxing();
+        unboxing();
+    }
+
+    private static void boxing(){
+        int i1 = 65;
+        Integer i2 = i1; //autoboxing
+        System.out.println(i2);
+
+        i2 = Integer.valueOf(i1);//boxing
+        System.out.println(i2);
+    }
+}
