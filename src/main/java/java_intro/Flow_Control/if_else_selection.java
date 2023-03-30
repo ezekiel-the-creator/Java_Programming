d7f5c7f1a363318c3b70af914f5db06296a826f1
Zak-Rey <zaktripple@gmail.com>
2023-03-30 08:26:46 +0300

    if else selection

--- src/main/java/java_intro/Flow_Control/if_else_selection.java
+++ src/main/java/java_intro/Flow_Control/if_else_selection.java
@@ -2,7 +2,7 @@ package java_intro.Flow_Control;
 
 public class if_else_selection {
     public static void main(String... args){
-        int age = 20;
+        int age = 17;
         
         if (age >= 18){
             System.out.println("You are an adult.");
