6e47835ff5f38702b49397aeb4d13ca6db0e3838
Zak-Rey <zaktripple@gmail.com>
2023-03-31 20:00:57 +0300

    throw exception java

---
+++ src/main/java/exception_handling/Throws.java
@@ -0,0 +1,12 @@
+package exception_handling;
+
+public class Throws {
+    private static void trywithResources() {
+        Connection conn;
+        ResultSet rs;
+
+        try {
+            conn
+        }
+    }
+}
