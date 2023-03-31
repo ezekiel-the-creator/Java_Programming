f2f01ecdc20e2a9ae0af03fec81ad0f1e4c93b9f
Zak-Rey <zaktripple@gmail.com>
2023-03-31 18:47:11 +0300

    java exceptions framework

---
+++ src/main/java/exception_handling/framework.java
@@ -0,0 +1,10 @@
+package exception_handling;
+
+public class framework {
+    
+    public static void main(String... args){
+        catchException1();
+        catchException2();
+        catchException3();
+    }
+}
