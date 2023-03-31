0557e64a920d2a8b1b79a5e15e0f954a32a7f501
Zak-Rey <zaktripple@gmail.com>
2023-03-31 20:02:19 +0300

    throw exception java

--- src/main/java/exception_handling/Throws.java
+++ src/main/java/exception_handling/Throws.java
@@ -1,12 +1,43 @@
 package exception_handling;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Throws {
-    private static void trywithResources() {
+
+    private static void tryWithRecources() {
         Connection conn;
         ResultSet rs;
-
         try {
-            conn
+            conn = DriverManager.getConnection("dburl", "username", "password");
+            rs = conn.createStatement().executeQuery("select * from some_table");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try (conn; rs) {
+            while (rs.next()) {
+                //process the retrieved data
+            }
+        } catch (SQLException ex) {
+            //Do something
+            //The exception was probably caused by incorrect SQL statement
         }
     }
+
+    private static void throwsDemo() throws SQLException{
+        Connection conn = DriverManager.getConnection("dburl", "username", "password");
+        ResultSet rs = conn.createStatement().executeQuery("select * from some_table");
+
+        try (conn; rs) {
+            while (rs.next()) {
+                //process the retrieved data
+            }
+        } finally { }
+    }
+
+
 }
