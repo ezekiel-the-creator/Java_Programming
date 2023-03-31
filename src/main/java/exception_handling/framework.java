ed370c6f6d8cf51a98794bacf4f79ddc516e824f
Zak-Rey <zaktripple@gmail.com>
2023-03-31 18:51:37 +0300

    java exceptions framework

--- src/main/java/exception_handling/framework.java
+++ src/main/java/exception_handling/framework.java
@@ -7,4 +7,27 @@ public class framework {
         catchException2();
         catchException3();
     }
+
+    private static void method(String s){
+        if(s.equals("abc")){
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+
+    private static void catchException1(){
+        System.out.println();
+        try {
+            method(s:null);
+        } catch(Exception ex){
+            System.out.println("CatcException1():");
+            System.out.println(ex.getClass().getCanonicalName());
+            waitForStackTrace();
+            ex.printStackTrace();
+            if(ex instanceof NullPointerException){
+                
+            }
+        }
+    }
 }
