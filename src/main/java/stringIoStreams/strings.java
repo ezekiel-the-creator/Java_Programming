8aec438fc764bae6e7f503691ee8ccd97e46b042
Zak-Rey <zaktripple@gmail.com>
2023-04-05 06:59:45 +0300

    Strings

--- src/main/java/stringIoStreams/strings.java
+++ src/main/java/stringIoStreams/strings.java
@@ -1,5 +1,51 @@
 package stringIoStreams;
 
 public class strings {
-    
+    public static void main(String... args){
+        //class string methods
+
+        equals();
+        contains();
+        startsEndsWith();
+        length();
+        indexOf();
+        substring();
+        concat();
+        join();
+        compareTo();
+        matches();
+        format();
+        replace();
+        toLowerCase();
+        toUpperCase();
+        split();
+        isEmpty();
+        valueOf();
+        repeat();
+        isBlank();
+        strip();
+        lines();
+
+        //String utils
+        trim();
+    }
+
+    private static void equals() {
+        String s1 = "abc";
+        String s2 = "abc";
+        String s3 = "acb";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println("abc".equals(s2));
+        System.out.println("abc".equals(s3));
+    }
+
+    private static void contains(){
+        String s6 = "abc43%";
+        String s7 = "43";
+        String s8 = "xyz";
+        System.out.println(s6.contains(s7));
+        System.out.println(s6.contains(s8));
+    }
 }
