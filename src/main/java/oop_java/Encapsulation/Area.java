52ef9b47810eaaef4d71baae2bda10333ad1e37e
Zak-Rey <zaktripple@gmail.com>
2023-04-06 20:34:10 +0300

    java encapsulation

---
+++ src/main/java/oop_java/Encapsulation/Area.java
@@ -0,0 +1,12 @@
+package oop_java.Encapsulation;
+
+public class Area {
+    //fields to calculate area
+    int length;
+    int width;
+
+    Area(int length, int breadth) {
+        this.length = length;
+        this.width = width;
+    }
+}
