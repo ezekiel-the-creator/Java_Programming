4f5449665208139533d6107897f2d57ac67bb7b0
Zak-Rey <zaktripple@gmail.com>
2023-04-06 20:37:31 +0300

    java encapsulation

--- src/main/java/oop_java/Encapsulation/Main.java
+++ src/main/java/oop_java/Encapsulation/Main.java
@@ -3,6 +3,9 @@ package oop_java.Encapsulation;
 public class Main {
     public static void main(String... args){
         Person person = new Person();
+        Area rectangle = new Area(0, 0);
+        rectangle.length = 6;
+        rectangle.width = 2;
         person.setName("John");
         person.setAge(30);
         System.out.println("Name: " + person.getName());
