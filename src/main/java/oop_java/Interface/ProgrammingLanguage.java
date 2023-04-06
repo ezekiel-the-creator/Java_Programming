593b5dab932be85060b82f451a7c10b8a0a13a1f
Zak-Rey <zaktripple@gmail.com>
2023-04-06 18:56:48 +0300

    Interfaces in java

---
+++ src/main/java/oop_java/Interface/ProgrammingLanguage.java
@@ -0,0 +1,7 @@
+package oop_java.Interface;
+
+public class ProgrammingLanguage implements Language {
+    public void getName(String name) {
+        System.out.println("Programming Language: " + name);
+    }
+}
