b9105f8f6db8a836d41e259df32aa0ce32aeb6ba
Zak-Rey <zaktripple@gmail.com>
2023-04-06 17:24:21 +0300

    Abstract classes and methods java

---
+++ src/main/java/oop_java/abstraction/Language.java
@@ -0,0 +1,11 @@
+package oop_java.abstraction;
+
+public abstract class Language {
+   public void display() {
+    System.out.println("This is java programming");
+   }
+}
+
+class Main extends Language {
+    public class static
+}
