a5ba536c67f32f0066b44f7cd3a84088fab640fe
Zak-Rey <zaktripple@gmail.com>
2023-04-06 18:35:06 +0300

    Abstraction java

--- src/main/java/oop_java/abstraction/Main.java
+++ src/main/java/oop_java/abstraction/Main.java
@@ -4,9 +4,13 @@ public class Main extends Language{
     public static void main(String... args){
         Main obj = new Main();
         Dog delta = new Dog();
+        MountainBike m1 = new MountainBike();
+        SportsBike s1 = new SportsBike();
 
         obj.display();
         delta.makeSound();
         delta.eat();
+        s1.brake();
+        m1.brake();
     }
 }
