159df648d4c833f102fe84972dc1c514bc84486d
Zak-Rey <zaktripple@gmail.com>
2023-04-08 09:46:45 +0300

    Switch case java

---
+++ src/main/java/java_intro/Flow_Control/Calculator.java
@@ -0,0 +1,43 @@
+package java_intro.Flow_Control;
+
+import java.util.Scanner;
+
+public class Calculator{
+	public static void main(String... args) {
+		int a;
+		int b;
+		char operand;
+		
+		
+		try(Scanner input = new Scanner(System.in)){
+			System.out.println("Enter the first number: ");
+			a = input.nextInt();
+			System.out.println("Enter the second number: ");
+			b = input.nextInt();
+			System.out.println("Enter the operator; +, -, *, /: ");
+			operand = input.next().charAt(0);
+		
+		switch(operand){
+			case '+' :
+			result = a + b;
+			break;
+			
+			case '-' :
+			result = a - b;
+			break;
+			
+			case '*':
+			result = a * b;
+			break;
+			
+			case '/':
+			result = a / b;
+			break;
+			
+			default :
+			System.out.println("Unknown operand");
+
+		}
+		System.out.println(a + " " + operand +" " +  b + "  =  "  + result);
+	}
+}	
