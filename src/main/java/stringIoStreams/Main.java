3529af36e39b730570cbc0321ea7dfbe336f9224
Zak-Rey <zaktripple@gmail.com>
2023-04-11 20:49:05 +0300

    java input Stream class

---
+++ src/main/java/stringIoStreams/Main.java
@@ -0,0 +1,25 @@
+package stringIoStreams;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+class Main {
+    public static void main(String... args){
+        byte[] array = new byte[100];
+
+        try {
+            InputStream input = new FileInputStream("java.txt");
+            System.out.println("Available bytes in the file: " + input.available());
+
+            input.read(array);
+            System.out.println("Data read from the file: ");
+
+            String data = new String(array);
+            System.out.println(data);
+
+            input.close();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+}
