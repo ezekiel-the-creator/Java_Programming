ea397bef631b2ce5dfa4c418aeeb0d187ce7d424
Zak-Rey <zaktripple@gmail.com>
2023-04-11 21:03:07 +0300

    java output Stream class

--- src/main/java/stringIoStreams/OutputStreamClass.java
+++ src/main/java/stringIoStreams/OutputStreamClass.java
@@ -13,6 +13,11 @@ public class OutputStreamClass {
             OutputStream out = new FileOutputStream("output.txt");
 
             byte[] dataBytes = data.getBytes();
+            out.write(dataBytes);
+            System.out.println("Data is written to the file");
+            out.close();
+        } catch(Exception e) {
+            e.getStackTrace();
         }
     }
 }
