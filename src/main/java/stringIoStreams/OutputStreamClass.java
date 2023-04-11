af97b41ee683fa10b7f8da07132899488b86473d
Zak-Rey <zaktripple@gmail.com>
2023-04-11 21:00:02 +0300

    java output Stream class

--- src/main/java/stringIoStreams/OutputStreamClass.java
+++ src/main/java/stringIoStreams/OutputStreamClass.java
@@ -3,4 +3,16 @@
 //FileOutputStream, ByteArrayStream, ObjectOutputStream
 package stringIoStreams;
 
-import 
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+
+public class OutputStreamClass {
+    public static void main(String... args){
+        String data = "This is java.";
+        try {
+            OutputStream out = new FileOutputStream("output.txt");
+
+            byte[] dataBytes = data.getBytes();
+        }
+    }
+}
