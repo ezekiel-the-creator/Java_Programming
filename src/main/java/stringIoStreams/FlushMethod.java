8b44ae0c012fb706e0eae291d4f01dd635124292
Zak-Rey <zaktripple@gmail.com>
2023-04-12 18:14:03 +0300

    File output Stream class Flush  method

--- src/main/java/stringIoStreams/FlushMethod.java
+++ src/main/java/stringIoStreams/FlushMethod.java
@@ -1,1 +1,6 @@
 //flush () method is used to clear the output stream, it forces the output stream to write all data to the destionation
+
+package stringIoStreams;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
