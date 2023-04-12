e0f3fa09200be5532de4d4470d2db6468f6a98f8
Zak-Rey <zaktripple@gmail.com>
2023-04-12 17:55:10 +0300

    File input stream method read

--- src/main/java/stringIoStreams/AvailableMethod.java
+++ src/main/java/stringIoStreams/AvailableMethod.java
@@ -3,5 +3,15 @@ package stringIoStreams;
 import java.io.FileInputStream;
 
 public class AvailableMethod {
-    
+    public static void main (String... args){
+        try{
+            FileInputStream input = new FileInputStream("input.txt");
+
+            System.out.println("Available bytes at the beggining: " + input.available());
+
+            input.read();
+            input.read();
+            input.read();
+        }
+    }
 }
