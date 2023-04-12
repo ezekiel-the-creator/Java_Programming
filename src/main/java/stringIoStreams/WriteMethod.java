73191b1c37cb10f082c52d87993a02535c53b27f
Zak-Rey <zaktripple@gmail.com>
2023-04-12 18:09:07 +0300

    File output Stream class Write method

--- src/main/java/stringIoStreams/WriteMethod.java
+++ src/main/java/stringIoStreams/WriteMethod.java
@@ -2,3 +2,13 @@ package stringIoStreams;
 
 import java.io.FileOutputStream;
 
+public class Main {
+    public static void main(String... args) {
+        String data = "This is a line of text inside the file";
+        try {
+            FileOutputStream output = new FileOutputStream("Output.txt");
+
+            byte[] array = data.getBytes();
+        }
+    }
+}
