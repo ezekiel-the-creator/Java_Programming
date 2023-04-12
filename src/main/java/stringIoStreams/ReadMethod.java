fac0d064828cf0f76dffa8dfb78f9b22319bd4cb
Zak-Rey <zaktripple@gmail.com>
2023-04-12 17:50:59 +0300

    File input stream method read

--- src/main/java/stringIoStreams/ReadMethod.java
+++ src/main/java/stringIoStreams/ReadMethod.java
@@ -1,7 +1,22 @@
 package stringIoStreams;
+import java.io.FileInputStream;
 
 public class ReadMethod {
     public static void main(String... args){
-        
+        try {
+            FileInputStream input = new FileInputStream("input.txt");
+            System.out.println("Data in the file: ");
+            int i = input.read();
+
+            while( i != -1){
+                System.out.print((char)i);
+
+                i = input.read();
+            }
+            input.close();
+        }
+        catch(Exception e){
+            e.getStackTrace();
+        }
     }
 }
