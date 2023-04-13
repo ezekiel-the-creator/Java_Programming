982a09788b888fea4d8f129088587477cd9c53f9
Zak-Rey <zaktripple@gmail.com>
2023-04-13 18:24:18 +0300

    Java Static Nested Class

--- src/main/java/stringIoStreams/Java_Nested_Static_Class/Main.java
+++ src/main/java/stringIoStreams/Java_Nested_Static_Class/Main.java
@@ -1,10 +1,14 @@
 package stringIoStreams.Java_Nested_Static_Class;
 
 public class Main {
-    Animal animal = new Animal();
+    public static void main(String... args){
+        Animal animal = new Animal();
 
     Animal.Reptile reptile = animal.new Reptile();
 
     //creating object of the static nested class
     Animal.Mammal mammal = new Animal.Mammal();
+    reptile.displayInfo();
+    
+    }
 }
