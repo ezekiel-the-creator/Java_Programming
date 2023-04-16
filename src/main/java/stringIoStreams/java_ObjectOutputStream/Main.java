9b0aea4b95b5adb780abc8dfcf908f8a91293cfd
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:18:47 +0300

    Object output Stream

--- src/main/java/stringIoStreams/java_ObjectOutputStream/Main.java
+++ src/main/java/stringIoStreams/java_ObjectOutputStream/Main.java
@@ -33,6 +33,9 @@ public class Main {
             System.out.println("Integer data :" + objStream.readInt());
             System.out.println("String data: " + objStream.readObject());
 
+            System.out.println("Dog Name: " + newDog.name);
+            System.out.println("Dog Breed: " + newDog.breed);
+
             output.close();
             objStream.close();
         }
