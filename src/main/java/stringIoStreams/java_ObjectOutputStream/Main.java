bd1dd6144e191f9e757a7fa58e8e13a91678eabd
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:10:38 +0300

    Object output stream

--- src/main/java/stringIoStreams/java_ObjectOutputStream/Main.java
+++ src/main/java/stringIoStreams/java_ObjectOutputStream/Main.java
@@ -6,5 +6,35 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        int data1 = 5;
+        String data2 = "This is programiz";
+
+        try {
+
+            FileOutputStream file = new FileOutputStream("file.txt");
+
+            // Creates an ObjectOutputStream
+            ObjectOutputStream output = new ObjectOutputStream(file);
+
+            // writes objects to output stream
+            output.writeInt(data1);
+            output.writeObject(data2);
+
+            // Reads data using the ObjectInputStream
+            FileInputStream fileStream = new FileInputStream("file.txt");
+            ObjectInputStream objStream = new ObjectInputStream(fileStream);
+
+            System.out.println("Integer data :" + objStream.readInt());
+            System.out.println("String data: " + objStream.readObject());
+
+            output.close();
+            objStream.close();
+        }
+
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
 }
