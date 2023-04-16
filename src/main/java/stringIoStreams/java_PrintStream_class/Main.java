913e1baf9c786b4dde752cc9b1758f3249927025
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:45:56 +0300

    java Printstream class

--- src/main/java/stringIoStreams/java_PrintStream_class/Main.java
+++ src/main/java/stringIoStreams/java_PrintStream_class/Main.java
@@ -5,11 +5,13 @@ import java.io.PrintStream;
 public class Main {
      public static void main(String... args){
         String data = "This is a text inside the file";
+        int age = 20;
 
         try {
             PrintStream output = new PrintStream("output.txt");
 
             output.print(data);
+            output.printf("I am %d years old", age);
             output.close();
         } catch (Exception e){
             e.getStackTrace();
