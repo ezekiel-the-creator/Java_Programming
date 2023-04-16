08f205f118d788fe232e3ab415cba48625784900
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:25:40 +0300

    java buffered input stream

--- src/main/java/stringIoStreams/java_BufferedInputStream/Main.java
+++ src/main/java/stringIoStreams/java_BufferedInputStream/Main.java
@@ -3,6 +3,15 @@ package stringIoStreams.java_BufferedInputStream;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 
-public class Main {
-    
+import oop_java.FinalKeyword.finalclass;
+
+public class Main { 
+      public static void main(String... args) {
+        try {
+            FileInputStream file = new FileInputStream("java.txt");
+            BufferedInputStream input = new BufferedInputStream(file);
+
+            
+        }
+      }
 }
