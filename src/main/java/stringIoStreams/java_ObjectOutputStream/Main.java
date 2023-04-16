e16b6148d850a37b336bff07899b10ade53a3490
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:09:25 +0300

    Object input stream

---
+++ src/main/java/stringIoStreams/java_ObjectOutputStream/Main.java
@@ -0,0 +1,10 @@
+package stringIoStreams.java_ObjectOutputStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Main {
+    
+}
