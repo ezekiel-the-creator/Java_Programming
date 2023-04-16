95a3d3ee28afab79ff30c323516e35e83d2e8bf1
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:42:25 +0300

    java Printstream class

--- src/main/java/stringIoStreams/java_PrintStream_class/Main.java
+++ src/main/java/stringIoStreams/java_PrintStream_class/Main.java
@@ -3,5 +3,16 @@ package stringIoStreams.java_PrintStream_class;
 import java.io.PrintStream;
 
 public class Main {
-        
+     public static void main(String... args){
+        String data = "This is a text inside the file";
+
+        try {
+            PrintStream output = new PrintStream("output.txt");
+
+            output.print(data);
+            output.close();
+        } catch (Exception e){
+            e.getStackTrace();
+        }
+     }   
 }
