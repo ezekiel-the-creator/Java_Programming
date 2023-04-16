621bf884743d0bf5583fc5e93ecc1d8b10ce4718
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:12:57 +0300

    Object output Stream

---
+++ src/main/java/stringIoStreams/java_ObjectOutputStream/Dog.java
@@ -0,0 +1,11 @@
+package stringIoStreams.java_ObjectOutputStream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+    
+}
