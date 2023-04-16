cad31d453b8fd198145ab8ebd47371b8f93102e6
Zak-Rey <zaktripple@gmail.com>
2023-04-16 11:14:09 +0300

    Object output Stream

--- src/main/java/stringIoStreams/java_ObjectOutputStream/Dog.java
+++ src/main/java/stringIoStreams/java_ObjectOutputStream/Dog.java
@@ -7,5 +7,11 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Dog implements Serializable {
-    
+    String name;
+    String breed;
+
+    public Dog(String name, String breed){
+        this.name = name;
+        this.breed = breed;
+    }
 }
