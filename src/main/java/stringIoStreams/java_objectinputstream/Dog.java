59719a85149f3269222c09c1c37ec0d972d25c4f
Zak-Rey <zaktripple@gmail.com>
2023-04-16 10:46:51 +0300

    Object input stream

---
+++ src/main/java/stringIoStreams/java_objectinputstream/Dog.java
@@ -0,0 +1,13 @@
+package stringIoStreams.java_objectinputstream;
+
+import java.io.Serializable;
+
+public class Dog {
+    String name;
+    String breed;
+    
+    public Dog(String name, string breed){
+        this.name = name;
+        this.breed = breed;
+    }
+}
