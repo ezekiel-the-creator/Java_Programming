5d41deb01269dea85246327184054a522c82fa62
Zak-Rey <zaktripple@gmail.com>
2023-05-04 14:24:15 +0300

    Exception handling

---
+++ src/main/java/exception_handling/Main.java
@@ -0,0 +1,8 @@
+package exception_handling;
+
+class Main {
+    public static void main(String[] args){
+        JavaTryCatchFinally list = new JavaTryCatchFinally();
+        list.writeList();
+    }
+}
