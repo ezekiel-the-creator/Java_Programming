504b1b847245eab756159193c68af86fb6666e4d
Zak-Rey <zaktripple@gmail.com>
2023-08-18 22:59:03 +0300

    Vectore Java

---
+++ src/main/java/java_List/Vector.java
@@ -0,0 +1,20 @@
+package java_List;
+import java.util.Vector;
+
+class JVector {
+    public static void main(String[] args){
+        Vector <String> mammals = new Vector<>();
+
+        mammals.add("Dog");
+        mammals.add("Horse");
+        mammals.add("Cat");
+        System.out.println("Vector: " + mammals);
+
+        //using addAll()
+        Vector <String> animals = new Vector<>();
+        animals.add("Crocodile");
+
+        animals.addAll(mammals);
+        System.out.println("New Vector: " + animals);
+    }
+}
