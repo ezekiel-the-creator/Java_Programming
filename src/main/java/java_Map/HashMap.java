e321d430c8be35c99ab88c30ce81ccc67f49b434
Zak-Rey <zaktripple@gmail.com>
2023-08-29 08:06:06 +0300

    Java Map

---
+++ src/main/java/java_Map/HashMap.java
@@ -0,0 +1,24 @@
+package java_Map;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class Main {
+    public static void main(String[] args) {
+        //Creating a map using the HashMappp
+        Map <String, Integer> numbers = new HashMap<>();
+
+        numbers.put("One", 1);
+        numbers.put("Two", 2);
+        System.out.println("Map: " + numbers);
+
+        System.out.println("Keys: " + numbers.keySet());
+
+        System.out.println("Values: " + numbers.values());
+
+        System.out.println("Entries: " + numbers.entrySet());
+
+        int value = numbers.remove("Two");
+        System.out.println("Removed Value: " + value);
+    }
+}
