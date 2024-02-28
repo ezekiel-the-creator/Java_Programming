5a257ff975a38cf413463220f72cd472f039b0df
ezekiel-the-creator <zaktripple@gmail.com>
2024-02-28 21:16:11 +0300

    28

--- src/main/java/stringIoStreams/Files.java
+++ src/main/java/stringIoStreams/Files.java
@@ -43,5 +43,23 @@ public class Files {
         }
     }
 
-    private 
+    private static void list1() {
+        String path1 = "demo1" + File.separator;
+        String path2 = "demo2" + File.separator;
+        File f1 = new File(path + "file1.txt");
+        File f2 = new File(path + "file2.txt");
+        File dir1 = new File(path1);
+        File dir = new File(path);
+
+        try {
+            dir.mkdirs();
+            f1.createNewFile();
+            f2.createNewFile();
+
+            System.out.print("\ndir1.list(): ");
+            for(String d: dir1.list()){
+                System.out.print(d + " ");
+            }
+        }
+    }
 }
