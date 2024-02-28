8f01dd9ac0e1ab19eb87b35e659c23331db10ed3
ezekiel-the-creator <zaktripple@gmail.com>
2024-02-28 05:56:28 +0300

    28

--- src/main/java/stringIoStreams/InputOutputStream.java
+++ src/main/java/stringIoStreams/InputOutputStream.java
@@ -22,6 +22,7 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class InputOutputStream {
+    private static ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
     private static void main(String... args) throws Exception {
         byteArrayInputStream1();
         byteArrayInputStream2();
@@ -48,11 +49,120 @@ public class InputOutputStream {
         scanner4();
     }
 
-    private static byte[] byteSource(){
+    private static byte[] bytesSource(){
         return new byte[] {42, 43, 44};
     }
 
-    private static void byteArrayInputStream() {
-        System.out.println("\nbyteArrayInputStream1()");
+    private static void byteArrayInputStream1() {
+        System.out.println("\nbyteArrayInputStream1():");
+        byte[] buffer = bytesSource();
+        try(ByteArrayInputStream bais = new ByteArrayInputStream(buffer)){
+            int data = bais.read();
+            while(data != -1) {
+                System.out.print(data + " ");
+                data = bais.read();
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private static void byteArrayInputStream2() {
+        System.out.println("\byteArrayInputStream2():");
+        byte[] buffer = bytesSource();
+        try(ByteArrayInputStream bais = new ByteArrayInputStream(buffer)){
+            int data;
+            while ((data = bais.read()) != -1) {
+                System.out.print(data + " ");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void byteArrayInputStream3() {
+        System.out.println("\byteArrayInputStream3():");
+        byte[] buffer = bytesSource();
+        try(ByteArrayInputStream bais = new ByteArrayInputStream(buffer)){
+            int data;
+            while((data = bais.read()) != -1) {
+                System.out.print(((char)data) + " ");
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private static void fileInputStream1() {
+        System.out.println("\nfileInputStream1():");
+        String file = classLoader.getResource("hello.txt").getFile();
+        try(FileInputStream fis = new FileInputStream(file)){
+            int data;
+            while ((data = fis.read()) != -1){
+                System.out.print(((char)data) + " ");
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+            pauseMs(100);
+        }
+    }
+
+    private static void fileInputstream2() {
+        System.out.println("\nfileInputStream2():");
+        String file = classLoader.getResource("hello.txt").getFile();
+        try(FileInputStream fis = new FileInputStream(file)){
+            int data;
+            while ((data = fis.read()) != -1) {
+                System.out.print((data) + " ");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private static void fileInputStream3() {
+        System.out.println("\nfileInputStream3():");
+        try(InputStream is = InputOutputStream.class.getResourceAsStream("/hello.txt")){
+            int data;
+            while ((data = is.read()) != -1) {
+                System.out.print((data) + " ");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private static class SomeClass implements Serializable {
+        private int field1 = 42;
+        private String field2 = "abc";
+    }
+    private static void objectInputOutputStream() {
+        
+        System.out.println("\nobjectInputOutputStream():");
+        String fileName = "someClass.bin";
+        try{
+            File file = new File(fileName);
+            if(!file.exists()){
+                file.createNewFile();
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        try (ObjectOutputStream objectOutputStream =
+                     new ObjectOutputStream(new FileOutputStream(fileName));
+             ObjectInputStream objectInputStream =
+                    new ObjectInputStream(new FileInputStream(fileName))){
+            SomeClass obj = new SomeClass();
+
+            objectOutputStream.writeObject(obj);
+
+            SomeClass objRead = (SomeClass) objectInputStream.readObject();
+
+            System.out.println(objRead.field1);
+            System.out.println(objRead.field2);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 }
