055abd22dead43657eb4c63e3ff281e8eb82cd2f
ezekiel-the-creator <zaktripple@gmail.com>
2024-02-28 21:02:54 +0300

    28

---
+++ src/main/java/stringIoStreams/Files.java
@@ -0,0 +1,16 @@
+package stringIoStreams;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.net.URI;
+
+public class Files {
+    public static void main(String... args) throws Exception {
+        createFile1();
+        createFile2();
+        list1();
+    }
+
+    
+}
