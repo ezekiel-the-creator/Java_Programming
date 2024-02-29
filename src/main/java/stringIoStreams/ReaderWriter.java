03e5f409837ab73a7b094f30ecb3204f1397356f
ezekiel-the-creator <zaktripple@gmail.com>
2024-02-29 22:33:17 +0300

    29

--- src/main/java/stringIoStreams/ReaderWriter.java
+++ src/main/java/stringIoStreams/ReaderWriter.java
@@ -3,3 +3,11 @@ package stringIoStreams;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.inputStreamReader;
+
+public class ReaderWriter {
+    public static void main(String... args) throws Exception {
+        bufferedReader();
+    }
+
+    priv
+}
