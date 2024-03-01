9e1bf0af4a548b383884024628d1a54442ff4ced
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-01 22:10:59 +0300

    1

---
+++ src/main/java/stringIoStreams/Person2.java
@@ -0,0 +1,11 @@
+package stringIoStreams;
+
+public class Person2 {
+    private int age;
+    private String name;
+
+    public Person2(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
