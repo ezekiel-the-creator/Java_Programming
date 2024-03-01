2b4e8b39d6aab2da32de4a2c5ae95c73c11afea6
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-01 21:51:12 +0300

    1

--- src/main/java/stringIoStreams/SerializableDemo.java
+++ src/main/java/stringIoStreams/SerializableDemo.java
@@ -5,6 +5,7 @@ import java.io.ObjectStreamField;
 
 public class SerializableDemo {
     public static void main(String... args){
-        
+        ObjectStreamClass osc1 = ObjectStreamClass.lookup(Person1.clas);
+        printInfo(osc1);
     }
 }
