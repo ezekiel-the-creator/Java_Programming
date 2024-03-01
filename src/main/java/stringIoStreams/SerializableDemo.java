87829b30ebd82aae2f7b3fbddf84fd21b938156f
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-01 21:49:46 +0300

    1

---
+++ src/main/java/stringIoStreams/SerializableDemo.java
@@ -0,0 +1,10 @@
+package stringIoStreams;
+
+import java.io.ObjectStreamClass;
+import java.io.ObjectStreamField;
+
+public class SerializableDemo {
+    public static void main(String... args){
+        
+    }
+}
