5ab78988edaf9736924008ac08e5a6c7e7842784
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-01 22:29:22 +0300

    Strings

--- src/main/java/stringIoStreams/String.java
+++ src/main/java/stringIoStreams/String.java
@@ -4,5 +4,29 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.List;
 
 public class String {
-    
+    public static void main(String... args) {
+        equals();
+        contains();
+        StartsEndsWith();
+        length();
+        indexOf();
+        substring();
+        concat();
+        join();
+        compareTo();
+        matches();
+        format();
+        replace();
+        toLowerUpperCase();
+        split();
+        isEmpty();
+        valuesOf();
+        repeat();
+        isBlank();
+        strip();
+        lines();
+
+        //stringUtils
+        trim();
+    }
 }
