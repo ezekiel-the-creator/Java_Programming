0438454fadeb210684e74c53e2d881803db4606f
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-01 22:08:35 +0300

    1

--- src/main/java/stringIoStreams/Person1.java
+++ src/main/java/stringIoStreams/Person1.java
@@ -4,4 +4,10 @@ import java.io.Serializable;
 
 public class Person1 implements Serializable {
     private int age;
+    private String name;
+
+    public Person1(int agae, String name) {
+        this.age = age;
+        this.name = name;
+    }
 }
