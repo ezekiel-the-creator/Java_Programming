6cfe0b25d9983cb369ea04d64e3f47ca89d82f55
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-05 11:42:53 +0300

    5/3 - Collection_Utilities

---
+++ src/main/java/Utilities/CollectionsDemo.java
@@ -0,0 +1,16 @@
+package Utilities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class CollectionsDemo {
+    
+}
