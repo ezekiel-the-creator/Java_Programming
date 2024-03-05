a13e82b67c01b55f6a8b47bcd8856eecaa200eba
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-05 11:23:12 +0300

    5/3

--- src/main/java/Utilities/ArraysUtils.java
+++ src/main/java/Utilities/ArraysUtils.java
@@ -4,5 +4,8 @@ import java.util.Arrays;
 import org.apache.commons.lang3.ArrayUtils;
 
 public class ArraysUtils {
-    
+    public static void main(String... args){
+        equalsDemo();
+        arrayUtils();
+    }
 }
