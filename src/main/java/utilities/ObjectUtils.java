f56935c7bba8fd5337eaa492b2aeff07f719f417
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-09 08:25:30 +0300

    09/03

--- src/main/java/utilities/ObjectUtils.java
+++ src/main/java/utilities/ObjectUtils.java
@@ -51,6 +51,42 @@ public class ObjectUtils {
             System.out.print(s + " "); //prints: s1 element was null
         }
     }
-    
 
+    private static class Person implements Comparable<Person> {
+        private int age;
+        private String name;
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+        public int getAge(){ return this.age; }
+        public String getName(){ return this.name; }
+        @Override
+        public int compareTo(Person p){
+            int result = Objects.compare(name, p.getName(),
+                    Comparator.naturalOrder());
+            if (result != 0) {
+                return result;
+            }
+            return Objects.compare(age, p.getAge(),
+                    Comparator.naturalOrder());
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null) return false;
+            if(!(o instanceof Person person)) return false;
+            return age == person.getAge() &&
+                    Objects.equals(name, person.getName()); //line 25
+        }
+        @Override
+        public int hashCode(){
+            return Objects.hash(age, name);
+        }
+        @Override
+        public String toString() {
+            return "Person{name=" + name + ", age=" + age + "}";
+        }
+    }
 }
+
