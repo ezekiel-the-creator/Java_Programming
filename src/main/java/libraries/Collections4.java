3c7674a173a9570059c33a0176990e0f1b93218e
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-14 23:15:42 +0300

    Mapping:

--- src/main/java/libraries/Collections4.java
+++ src/main/java/libraries/Collections4.java
@@ -30,4 +30,48 @@ public class Collections4 {
         System.out.println(bag);
         System.out.println(bag.getCount("one"));
     }
+
+    private static bidiMap(){
+        System.out.println("\nbidiMap():");
+
+        BidiMap<Integer, String> bidi = new TreeBidiMap<>();
+        bidi.put(2, "two");
+        bidi.put(3, "three");
+        System.out.println(bidi);
+        System.out.println(bidi.inverseBidiMap());
+        System.out.println(bidi.get(3));
+        System.out.println(bidi.getKey("three"));
+        bidi.removeValue("three");
+        System.out.println(bidi);
+    }
+
+    private static void iterableMap(){
+        System.out.println("\niterableMap():");
+
+        IterableMap<Integer, String> map = new HashedMap<>(Map.of(1, "one", 2, "two"));
+        MapIterator it = map.mapIterator();
+        while (it.hasNext()){
+            Object key = it.next();
+            Object value = it.getValue();
+            System.out.print(key + ", " + value + ", ");
+            if(((Integer)key) == 2){
+                it.setValue("three");
+            }
+        }
+        System.out.println("\n" + map);
+    }
+
+    private static void orderedMap(){
+        System.out.println("\norderedMap():");
+
+        OrderedMap<Integer, String> map = new LinkedMap<>();
+        map.put(4, "four");
+        map.put(7, "seven");
+        map.put(12, "twelve");
+
+        System.out.println(map.firstKey());
+        System.out.println(map.nextKey(2));
+        System.out.println(map.nextKey(7));
+        System.out.println(map.nextKey(4));
+    }
 }
