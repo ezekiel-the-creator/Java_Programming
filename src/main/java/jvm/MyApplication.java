1a4bb16fb22c5054c07172ec7e5ce8074d3b86a5
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-17 22:21:40 +0300

    17

--- src/main/java/jvm/MyApplication.java
+++ src/main/java/jvm/MyApplication.java
@@ -4,7 +4,7 @@ import jvm.example.ExampleClass;
 
 public class MyApplication {
     public static void main(String... args){
-        System.out.println("Hello, World! \n This is Java");
+        System.out.println("Hello, World! \n This is Java Programming");
         for(String arg: args) {
             System.out.print(arg + " ");
         }
