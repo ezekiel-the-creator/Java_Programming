7c8aeff31ec1a6066d43bc63129ef96bd3e32675
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-17 22:21:06 +0300

    17

---
+++ src/main/java/jvm/MyApplication.java
@@ -0,0 +1,17 @@
+package jvm;
+
+import jvm.example.ExampleClass;
+
+public class MyApplication {
+    public static void main(String... args){
+        System.out.println("Hello, World! \n This is Java");
+        for(String arg: args) {
+            System.out.print(arg + " ");
+        }
+        String p = System.getProperty("someParameter");
+        System.out.println("\n" + p);
+
+        int i = ExampleClass.multiplyByTwo(2);
+        System.out.println(i);
+    }
+}
