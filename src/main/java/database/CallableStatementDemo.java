053c0daa9e51165b016417452ccbeb77c05bf666
ezekiel-the-creator <zaktripple@gmail.com>
2024-03-18 13:33:49 +0300

    databaseImports

--- src/main/java/database/CallableStatementDemo.java
+++ src/main/java/database/CallableStatementDemo.java
@@ -1,5 +1,12 @@
 package database;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Properties;
+
 public class CallableStatementDemo {
     
 }
